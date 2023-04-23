bd3d204202f401d88287998c16d2ec11b49a8f41
Ekaterina Volobueva <madam.larinvae@yandex.ru>
2023-04-24 00:22:58 +0300

    localization and button arrangement

--- Expense Tracker/Source/Presentation/Modules/MainPage/MainPageViewController.swift
+++ Expense Tracker/Source/Presentation/Modules/MainPage/MainPageViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 protocol MainPageViewControllerProtocol: AnyObject {}
 
-final class MainPageViewController: UIViewController, MainPageViewControllerProtocol {
+final class MainPageViewController: UIViewController,
+                                    MainPageViewControllerProtocol {
     // MARK: - Public properties
     var presenter: MainPagePresenterProtocol?
-
+    
+    // MARK: - Private properties
+    
+    private lazy var myButton: UIButton = {
+        let myButton = UIButton()
+        myButton.setTitle(Strings.signUpButtonTitle, for: .normal)
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        myButton.backgroundColor = .systemBlue
+        myButton.setTitleColor(.white, for: .normal)
+        myButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        return myButton
+    }()
+    
     // MARK: - Init
     init(presenter: MainPagePresenterProtocol) {
         self.presenter = presenter
@@ -24,5 +37,22 @@ final class MainPageViewController: UIViewController, MainPageViewControllerProt
     // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(myButton)
+        setupAutoLayout()
+    }
+    
+    // MARK: - Private func
+    
+    private func setupAutoLayout() {
+        NSLayoutConstraint.activate([
+            myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            myButton.widthAnchor.constraint(equalToConstant: 200),
+            myButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    @objc private func buttonTapped() {
+        print("Button tapped!")
     }
 }
