537d03a3eeef1adb5b94f2dcd999a5a38d6040b5
Ekaterina Volobueva <madam.larinvae@yandex.ru>
2023-05-10 00:24:44 +0300

    CoordinatorProtocol & extension CoordinatorProtocol created

---
+++ Expense Tracker/Source/Services/Coordinator/CoordinatorProtocol.swift
@@ -0,0 +1,28 @@
+//
+//  CoordinatorProtocol.swift
+//  Expense Tracker
+//
+//  Created by Ekaterina Volobueva on 10.05.2023.
+//
+
+import UIKit
+
+protocol CoordinatorProtocol {
+    var navigationController: UINavigationController { get }
+    init(navigationController: UINavigationController)
+}
+
+extension CoordinatorProtocol {
+    func dismissController(animated: Bool) {
+        navigationController.dismiss(animated: animated)
+    }
+    func popController(animated: Bool) {
+        navigationController.popViewController(animated: animated)
+    }
+    func presentController(_ viewController: UIViewController, animated: Bool, completion: (() -> Void)?) {
+        navigationController.present(viewController, animated: animated, completion: completion)
+    }
+    func pushController(_ viewController: UIViewController, animated: Bool) {
+        navigationController.pushViewController(viewController, animated: animated)
+    }
+}
