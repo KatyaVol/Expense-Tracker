53ae633e9ad77081da7832a4656c502cf0fd536c
Ekaterina Volobueva <madam.larinvae@yandex.ru>
2023-05-17 17:05:57 +0300

    Empty CategoryViewController created

---
+++ Expense Tracker/Source/Presentation/Modules/CategoryViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryViewController.swift
+//  Expense Tracker
+//
+//  Created by Ekaterina Volobueva on 17.05.2023.
+//
+
+import UIKit
+
+class CategoryViewController {
+    
+    func test() {}
+}
